8028e7d5f20c158bd8b416357bf809931059d33c
jarlenandres <jaarojas@gmai.com>
2025-09-29 14:12:47 -0500

    Creating the QueueList class library

---
+++ Queue/QueueList/QueueUsingList.cs
@@ -0,0 +1,37 @@
+﻿namespace QueueList
+{
+    public class QueueUsingList<T>
+    {
+        private List<T> _elements;
+
+        public QueueUsingList()
+        {
+            _elements = [];
+        }
+
+        public void Enqueue(T item)
+        {
+            _elements.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_elements.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+            var item = _elements[0];
+            _elements.RemoveAt(0);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (_elements.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+            return _elements[0];
+        }
+    }
+}
