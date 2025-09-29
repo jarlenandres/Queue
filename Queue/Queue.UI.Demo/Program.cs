5bd867dec58972b6f5862e8a7f04d984a53bd58e
jarlenandres <jaarojas@gmai.com>
2025-09-29 16:13:43 -0500

    Creating a demo program for silultaneous queue and dequeue

---
+++ Queue/Queue.UI.Demo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+internal class Program
+{
+    private static ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+    private static CancellationTokenSource cts = new CancellationTokenSource();
+    private static Random random;
+    private static void Main(string[] args)
+    {
+        random = new Random();
+        var producerTask = Task.Run(() => Producer(cts.Token));
+        var consumerTask = Task.Run(() => Consumer(cts.Token));
+
+        Console.WriteLine("Press any key to stop...");
+        Console.ReadKey();
+        cts.Cancel();
+
+        Task.WaitAll(producerTask, consumerTask);
+        Console.WriteLine("Task completed. Exiting...");
+    }
+
+    public static void Producer(CancellationToken token)
+    {
+        int counter = 0;
+        while (!token.IsCancellationRequested)
+        {
+            counter ++;
+            queue.Enqueue(counter);
+            Console.WriteLine($"Enqueue: {counter}");
+            Thread.Sleep(random.Next(1000, 5000));
+        }
+    }
+    
+    public static void Consumer(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            if (queue.TryDequeue(out int item))
+            {
+                Console.WriteLine($"\tDequeue: {item}");
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty...");
+            }
+            Thread.Sleep(random.Next(1000, 5000));
+        }
+    }
+}
